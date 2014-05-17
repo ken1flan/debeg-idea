9c68b629b665f21458fc4af3abd39ef8f4812764
ken1flan <ken1flan@gmail.com>
2014-05-17 13:27:24 +0900

    Likeモデルの追加

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,7 @@
 class Idea < ActiveRecord::Base
   has_many :comments
   belongs_to :user
+  has_many :likes, as: :likable
 
   validates :title, length: { maximum: 100 }, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140427143623) do
+ActiveRecord::Schema.define(version: 20140517034628) do
 
   create_table "comments", force: true do |t|
     t.integer  "idea_id"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20140427143623) do
   end
 
   add_index "ideas", ["user_id"], name: "index_ideas_on_user_id"
+
+  create_table "likes", force: true do |t|
+    t.string   "likable_type"
+    t.integer  "likable_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "provider",   null: false
